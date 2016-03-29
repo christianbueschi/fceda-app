4ede55d85d0326045deb4883f9988ad5a0f8cd73
Christian Bueschi <christian.bueschi@namics.com>
2016-03-29 21:28:36 +0200

    add pns registration

--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -1,5 +1,6 @@
-import {Page} from 'ionic-angular';
+import {Page, NavController} from 'ionic-angular';
 import {NewsPage} from '../news/news';
+import {NewsdetailPage} from '../newsdetail/newsdetail';
 import {TablePage} from '../table/table';
 import {GamesPage} from '../games/games';
 import {TeamPage} from '../team/team';
@@ -17,4 +18,34 @@ export class TabsPage {
   tab3Root: any = GamesPage;
   tab4Root: any = TeamPage;
   tab5Root: any = LivetickerPage;
+
+  public nav: NavController;
+
+  constructor(nav: NavController) {
+    this.initOneSignal();
+    this.nav = nav;
+  }
+
+
+  initOneSignal() {
+    
+    document.addEventListener('deviceready', () => {
+   
+      window.plugins.OneSignal.init(
+        "0e694cca-15b1-42b5-817e-151c9f3a3d70",
+        {googleProjectNumber: "703322744261"},
+        this.onNotificationOpened.bind(this)
+      );
+  
+      // Show an alert box if a notification comes in when the user is in your app.
+      window.plugins.OneSignal.enableInAppAlertNotification(true);
+    }, false);
+    
+  }
+
+  onNotificationOpened() {
+    this.nav.push(LivetickerPage);
+  }
+
+
 }
